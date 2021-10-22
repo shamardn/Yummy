f771559309ccb2be001400e3fafd2cfca0121919
Shamardn <waelshamardn@yahoo.com>
2021-10-22 21:32:15 +0200

    Adding the Yummy app repo

---
+++ app/src/main/java/com/example/yummy/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.yummy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
